16eaa1f916c403c1118240f1f5208981e3108e55
Aaron Schiff <aaron@schiff.co.nz>
2016-01-19 12:04:12 +1300

    Display tweaks including font size and gridlines

--- clean-ggplot-theme.R
+++ clean-ggplot-theme.R
@@ -2,30 +2,39 @@ library(ggplot2)
 library(ggthemes)
 
 # Basic elements of a clean ggplot theme
-mytheme = function(base_size = 10, base_family = "sans") {
+mytheme = function(base_size = 12, base_family = "sans") {
   (
     theme_foundation(base_size = base_size, base_family = base_family) + 
       theme(
         # Plot
-        plot.title = element_text(face = "bold", vjust = 1), 
-        plot.title = element_text(vjust = 1, size = 10), 
+        plot.title = element_text(face = "bold", size = 12, hjust = 0), 
+        plot.margin = unit(c(5, 5, 5, 5), "mm"), 
         
         # Axes
         axis.line.y = element_blank(), 
         axis.line.x = element_blank(), 
         axis.ticks = element_line(colour = "#666666"), 
         axis.ticks.y = element_blank(), 
-        axis.title = element_text(face = "italic"), 
-        axis.title.y = element_text(vjust = 1, hjust = 0.1), 
-        axis.title.x = element_text(hjust = 1, vjust = 0.1), 
-        axis.text.x = element_text(vjust = 0), 
+        axis.title = element_text(face = "italic", size = 12), 
+        axis.title.y = element_text(vjust = 0.95, angle = 0), 
+        axis.title.x = element_text(hjust = 1), 
+        axis.text = element_text(size = 11), 
         
         # Panel
-        panel.grid.major.y = element_line(colour = "#f2f2f2"), 
+        panel.grid.major.y = element_line(colour = "#e2e2e2"), 
         panel.grid.major.x = element_blank(), 
-        panel.grid.minor = element_blank(), 
+        panel.grid.minor.y = element_line(colour = "#eeeeee"), 
+        panel.grid.minor.x = element_blank(), 
         panel.border = element_rect(colour = NA), 
-        panel.background = element_rect(colour = NA)
+        panel.background = element_rect(colour = NA), 
+        panel.margin = unit(4, "lines"), 
+        
+        # Facets
+        strip.background = element_blank()
       )
   )
 }
+
+# Testing
+bc = ggplot(pd, aes(x = value, y = count)) + geom_bar(stat = "identity") + ggtitle(y$results[[8]]$variable_name) + mytheme()
+print(bc)
