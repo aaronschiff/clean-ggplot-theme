34b9da30f18900126a6cbe59aaa8b478dc2631d9
Aaron Schiff <aaron@schiff.co.nz>
2017-07-19 10:29:08 +1200

    Tweaks and improvements; now allows pass-through of custom parameters.

--- clean-ggplot-theme.R
+++ clean-ggplot-theme.R
@@ -2,36 +2,46 @@ library(ggplot2)
 library(ggthemes)
 
 # Basic elements of a clean ggplot theme
-mytheme = function(base_size = 12, base_family = "sans") {
-  (
-    theme_foundation(base_size = base_size, base_family = base_family) + 
-      theme(
-        # Plot
-        plot.title = element_text(face = "bold", size = 12, hjust = 0), 
-        plot.margin = unit(c(5, 5, 5, 5), "mm"), 
-        
-        # Axes
-        axis.line.y = element_blank(), 
-        axis.line.x = element_blank(), 
-        axis.ticks = element_line(colour = "#666666"), 
-        axis.ticks.y = element_blank(), 
-        axis.title = element_text(face = "italic", size = 12), 
-        axis.title.y = element_text(vjust = 0.95, angle = 0), 
-        axis.title.x = element_text(hjust = 1), 
-        axis.text = element_text(size = 11), 
-        
-        # Panel
-        panel.grid.major.y = element_line(colour = "#e2e2e2"), 
-        panel.grid.major.x = element_blank(), 
-        panel.grid.minor.y = element_line(colour = "#eeeeee"), 
-        panel.grid.minor.x = element_blank(), 
-        panel.border = element_rect(colour = NA), 
-        panel.background = element_rect(colour = NA), 
-        panel.margin = unit(4, "lines"), 
-        
-        # Facets
-        strip.background = element_blank()
-      )
-  )
+clean_theme <- function(base_size = 12, base_family = "Helvetica", ...) {
+  theme_foundation(base_size = base_size, base_family = base_family) + 
+    theme(
+      # Plot
+      plot.title = element_text(face = "bold", size = rel(1.1), hjust = 0), 
+      plot.margin = unit(c(5, 5, 5, 5), "mm"), 
+      plot.background = element_rect(fill = "white", colour = "white"), 
+      
+      # Axes
+      axis.line.y = element_blank(), 
+      axis.line.x = element_blank(), 
+      axis.ticks.x = element_line(colour = "#aaaaaa"), 
+      axis.ticks.y = element_blank(), 
+      axis.title = element_text(size = rel(1)), 
+      axis.title.y = element_text(vjust = 0.95, angle = 0), 
+      axis.title.x = element_text(hjust = 1, margin = unit(c(1.5, 0, 0, 0), "lines")), 
+      axis.text = element_text(size = rel(0.9)), 
+      
+      # Gridlines
+      panel.grid.major.y = element_line(colour = "#e8e8e8"), 
+      panel.grid.major.x = element_blank(), 
+      panel.grid.minor.y = element_blank(), 
+      panel.grid.minor.x = element_blank(), 
+      
+      # Panel elements
+      panel.border = element_blank(), 
+      panel.background = element_rect(colour = "white"), 
+      panel.spacing = unit(2, "lines"), 
+      
+      # Facets
+      strip.background = element_blank(), 
+      strip.text = element_text(face = "bold", hjust = 0, size = rel(1)),
+      
+      # Legend
+      legend.key = element_blank(), 
+      legend.text = element_text(size = rel(1)), 
+      legend.title = element_text(face = "bold", size = rel(1)), 
+      
+      # Pass-through
+      ...
+    )
 }
 
